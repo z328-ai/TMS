5a4bffdb98dd4c7dfb13744f8f80c962fc655dd9
z328-ai <627389015@qq.com>
2023-10-26 22:24:21 +0800

    s

---
+++ TMS_END/src/main/java/com/example/tms_end/TmsEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.tms_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmsEndApplication.class, args);
+    }
+
+}
---
+++ TMS_END/src/test/java/com/example/tms_end/TmsEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tms_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
